fd82381c1e33edb2d7487226b8b10f5716ecec1f
sjd58 <dicksonsj0798@gmail.com>
2022-03-09 16:25:32 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,9 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix"><div>Good morning</div>
-    </div>
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
     );
   }
 }
