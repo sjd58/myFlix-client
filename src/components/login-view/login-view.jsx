cd37d11192f75f65dcac0cc777537e5df69a08cc
sjd58 <dicksonsj0798@gmail.com>
2022-03-24 22:23:19 -0400

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,37 +1,89 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types'; //do I need this? check the code examples carefully
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
+import axios from 'axios';
 import './login-view.scss';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
+  // Declare hook for each input
+  const [ usernameErr, setUsernameErr ] = useState('');
+  const [ passwordErr, setPasswordErr ] = useState('');
+
+// validate user inputs
+const validate = () => {
+  let isReq = true;
+  if(!username){
+    setUsernameErr('Username Required');
+    isReq = false;
+  } else if(username.length < 2){
+    setUsernameErr('Username must be longer than 2 characters');
+    isReq = false;
+  }
+  if(!password){
+    setPasswordErr('Password Required');
+    isReq = false;
+  } else if(password.length < 6){
+    setPassword('Password must be longer than 6 characters')
+    isReq = false;
+  }
+  return isReq;
+}
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    const isReq = validate();
+    if(isReq) {
+      /* Send a request to the server for authentication */
+      axios.post('https://myflixapi-by-sjd58.herokuapp.com/login', {
+        Username: username,
+        Password: password
+      })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
+    }
   };
 
   return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username: </Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password: </Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form>
+    <Container id="login-form">
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card id="login-card">
+              <Card.Body>
+                <Card.Title id="login-card-title">Please login to MyFlix</Card.Title>
+                <Form>
+                  <Form.Group controlId="formUsername">
+                    <Form.Label>Username: </Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+                      {usernameErr && <p>{usernameErr}</p>}
+                  </Form.Group>
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password: </Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                      {passwordErr && <p>{passwordErr}</p>}
+                  </Form.Group>
+                  <Button variant="primary" type="submit" onClick={handleSubmit}>
+                    Submit
+                  </Button>
+                </Form>
+                <Card.Text>Not registered yet?</Card.Text>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
+
+// Need to set up button to link to registration page
 
 LoginView.propTypes = {
   register: PropTypes.shape({
