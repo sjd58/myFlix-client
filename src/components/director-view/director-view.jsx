cf455f7077b1942534d1f5803e556bcc94661de4
sjd58 <dicksonsj0798@gmail.com>
2022-03-28 11:35:03 -0400

    Update director-view.jsx

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { Container, Row, Col, Card, Button, CardGroup } from 'react-bootstrap';
+import { Row, Col, Card, Button, CardGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './director-view.scss'
 
 export class DirectorView extends React.Component {
+
   render() {
     const { director, movies, onBackClick } = this.props;
 
     return (
-      <Container>
-        <Row>
-          <Col>
-          <Card id="director-view">
-            <Card.Body>
-              <Card.Title>{director.Name}</Card.Title>
-              <Card.Text>{director.Bio}</Card.Text>
-              <Card.Text>{director.Birth}</Card.Text>
-              <Card.Text>{director.Death}</Card.Text>
-              <Button id="director-back-button" onClick={() => {onBackClick();}}></Button>
-            </Card.Body>
-          </Card>
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Col>
+        <Card id="director-view">
+          <Card.Body>
+            <Card.Title>{director.Name}</Card.Title>
+            <Card.Text>{director.Bio}</Card.Text>
+            <Card.Text>Birth: {director.Birth}</Card.Text>
+            <Card.Text>Death: {director.Death}</Card.Text>
+            <Button id="director-back-button" onClick={() => {onBackClick();}}>Back</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+      </Row>
     )
   }
 }
