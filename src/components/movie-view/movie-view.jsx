e358565d33602ad0ea31d08a5a25cc4776cdd1d2
sjd58 <dicksonsj0798@gmail.com>
2022-03-29 12:26:41 -0400

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,11 +1,70 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import { Card, Col, Row, Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
 import "./movie-view.scss";
 
 export class MovieView extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state= {
+      FavoriteMovies: [],
+      userDetails: []
+    }
+
+    this.getUserDetails = this.getUserDetails.bind(this);
+    this.addFavorite = this.addFavorite.bind(this);
+  }
+
+  componentDidMount() {
+    const accessToken = localStorage.getItem('token');
+    this.getUserDetails(accessToken);
+  }
   
+  getUserDetails() {
+    const Username = localStorage.getItem('user');
+    const token = localStorage.getItem('token');
+    axios.get(`https://myflixapi-by-sjd58.herokuapp.com/users/${Username}`, 
+    {
+      headers: { Authorization: `Bearer ${token}`}
+    }
+  )
+  .then((response) => {
+    console.log(response.data);
+      this.props.setUser(response.user);
+
+      this.setState({
+        userDetails: response.data,
+        FavoriteMovies: response.data.FavoriteMovies
+      });
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  addFavorite = (e, movie) => {
+    e.preventDefault();
+    const Username = localStorage.getItem('user');
+    const token = localStorage.getItem('token');
+
+    axios.post(`https://myflixapi-by-sjd58.herokuapp.com/users/${Username}/movies/${movie._id}`,
+      {
+        headers: { Authorization: `Bearer ${token}`}
+      }
+    )
+    .then((response) => {
+      console.log(response);
+      alert("Favorite has been added!");
+      this.componentDidMount();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
     const { movie, onBackClick } = this.props;
 
@@ -27,19 +86,20 @@ export class MovieView extends React.Component {
                   <Button variant="link">Genre</Button>
                 </Link>
               </Card.Body>
+              <Card.Footer>
+                <Button id="movie-view-button" onClick={() => { onBackClick(); }}>Back</Button>
+                <Button id="movie-view-button" onClick={(e) => { this.addFavorite(e, movie)}}>Add to favorites</Button>
+              </Card.Footer>
             </Card>
-            <Button id="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button>
-            <Button id="movie-view-button" onClick={() => {}}>Add to favorites</Button>
           </Col>
         </Row>
       </Container>
-    
     );
   }
 }
 
 MovieView.propTypes = {
-  movie: PropTypes.shape({
+  movies: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
